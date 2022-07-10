525a6050bb06951257a5a44b54fadffd29b18229
EC2 Default User <aya420tansk666@gmail.com>
2022-07-10 05:47:10 +0000

    非ログインTopPageとログイン後TopPageを作成

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,16 +1,11 @@
-<!DOCTPE html>
-<heml lang="ja">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>ログイン機能付きタスク管理サービス</title>
-    </head>
-    <body>
+@extends('layout')
+
+{{-- メインコンテンツ --}}
+@section('contets')
         <h1>ログイン</h1>
-        <form action="./top.html" method="post">
+        <form action="/login" method="post">
             email：<input name="email"><br>
-            パスワード：<input name="password" type="password"><br>
+            パスワード：<input  name="password" type="password"><br>
             <button>ログインする</button>
         </form>
-    </body>
-</heml>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,8 +18,9 @@ use App\Http\Controllers\AuthController;
 
 // タスク管理システム
 Route::get('/', [AuthController::class, 'index']);
+Route::get('/task/list', [TaskController::class,'list']);
 
 
 // テスト用
 Route::get('/welcome', [WelcomeController::class, 'index']);
-Route::get('/welcome/second', [WelcomeController::class, 'second']);git
+Route::get('/welcome/second', [WelcomeController::class, 'second']);
