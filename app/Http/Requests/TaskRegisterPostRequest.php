b7792605631aa70a70f3f6f0ca877c28d12dc1f7
EC2 Default User <aya420tansk666@gmail.com>
2022-07-27 11:18:52 +0000

    Procfile for Heroku

--- app/Http/Requests/TaskRegisterPostRequest.php
+++ app/Http/Requests/TaskRegisterPostRequest.php
@@ -4,13 +4,14 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
+use App\Models\Task as TaskModel;
 
 class TaskRegisterPostRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
+     * Get the validation rules that apply to the request.
      *
-     * @return bool
+     * @return array
      */
     public function rules()
     {
@@ -18,7 +19,7 @@ class TaskRegisterPostRequest extends FormRequest
             'name' => ['required', 'max:128'],
             'period' => ['required', 'date', 'after_or_equal:today'],
             'detail' => ['max:65535'],
-            'priority' => ['required', 'numeric', Rule::in([1, 2, 3])],
+            'priority' => ['required', 'numeric', Rule::in( array_keys(TaskModel::PRIORITY_VALUE) ) ],
             ];
     }
 }
