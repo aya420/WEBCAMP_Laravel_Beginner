bb46dfe2a4462f9ae58e0264aaedd57d1dc8dd12
EC2 Default User <aya420tansk666@gmail.com>
2022-07-30 03:15:40 +0000

    タスクの一覧出力を書いた

--- resources/views/task/list.blade.php
+++ resources/views/task/list.blade.php
@@ -6,41 +6,42 @@
 {{-- メインコンテンツ --}}
 @section('contets')
         <h1>タスクの登録</h1>
-        @if (session('front.task_register_success') == true)
-            タスクを登録しました!!<br>
-        @endif
-        @if ($errors->any())
-            <div>
+            @if (session('front.task_register_success') == true)
+                タスクを登録しました！！<br>
+            @endif
+            @if ($errors->any())
+                <div>
                 @foreach ($errors->all() as $error)
                     {{ $error }}<br>
                 @endforeach
-            </div>
-        @endif
-        　<form action="/task/register" method="post">
-        　    @csrf
-          　  タスク名:<input name="name" value="{{ old('name') }}"><br>
-            　期限:<input name="period" type="date" value="{{ old('period') }}"><br>
-            　タスク詳細:<textarea name="detail">{{ old('detail') }}</textarea><br>
-            　重要度:<label><input type="radio" name="priority" value="1" @if (old('priority') == 1) checked @endif>低い</label>/
-              　      <label><input type="radio" name="priority" value="2" @if (old('priority', 2) == 2) checked @endif>普通</label>/
-                　   <label><input type="radio" name="priority" value="3" @if (old('priority') == 3) checked @endif>高い</label><br>
-        　　　<button>タスクを登録する</button>
-    　　    </form>
+                </div>
+            @endif
+            <form action="/task/register" method="post">
+                @csrf
+                タスク名:<input name="name" value="{{ old('name') }}"><br>
+                期限:<input name="period" type="date" value="{{ old('period') }}"><br>
+                タスク詳細:<textarea name="detail">{{ old('detail') }}</textarea><br>
+                重要度:<label><input type="radio" name="priority" value="1" @if (old('priority') == 1) checked @endif>低い</label> / 
+                    <label><input type="radio" name="priority" value="2" @if (old('priority', 2) == 2) checked @endif>普通</label> / 
+                    <label><input type="radio" name="priority" value="3" @if (old('priority') == 3) checked @endif>高い</label><br>
+                <button>タスクを登録する</button>
+            </form>
+
         <h1>タスクの一覧(未実装)</h1>
         <a href="./top.html">CSVダウンロード(未実装)</a><br>
         <table border="1">
-            <tr>
-                <th>タスク名
-                <th>期限
-                <th>重要度
+        <tr>
+            <th>タスク名
+            <th>期限
+            <th>重要度
 @foreach ($list as $task)
-            <tr>
-                <td>{{ $task->name }}
-                <td>{{ $task->period }}
-                <td>{{ $task->getPriorityString() }}
-                <td><a href="./detail.html">詳細閲覧</a>
-                <td><a href="./edit.html">編集</a>
-                <td><form action="./top.html"><button>完了</button></form>
+        <tr>
+            <td>{{ $task->name }}
+            <td>{{ $task->period }}
+            <td>{{ $task->getPriorityString() }}
+            <td><a href="./detail.html">詳細閲覧</a>
+            <td><a href="./edit.html">編集</a>
+            <td><form action="./top.html"><button>完了</button></form>
 @endforeach
         </table>
         <!-- ページネーション -->
