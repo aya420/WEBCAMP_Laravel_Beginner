716afbab19e5bbef41d7b2178efa8ebb8a87541f
EC2 Default User <aya420tansk666@gmail.com>
2022-08-02 12:20:16 +0000

    タスクの編集を書いた

--- resources/views/task/list.blade.php
+++ resources/views/task/list.blade.php
@@ -1,4 +1,3 @@
-cd
 @extends('layout')
 
 {{-- タイトル --}}
@@ -23,7 +22,7 @@ cd
                 期限:<input name="period" type="date" value="{{ old('period') }}"><br>
                 タスク詳細:<textarea name="detail">{{ old('detail') }}</textarea><br>
                 重要度:<label><input type="radio" name="priority" value="1" @if (old('priority') == 1) checked @endif>低い</label> / 
-                    <label><input type="radio" name="priority" value="2" @if (old('priority', 2) == 2) checked @endif>普通</label> / 
+                    <label><input type="radio" name="priority" value="2" @if (old('priority') == 2) checked @endif>普通</label> / 
                     <label><input type="radio" name="priority" value="3" @if (old('priority') == 3) checked @endif>高い</label><br>
                 <button>タスクを登録する</button>
             </form>
@@ -41,7 +40,7 @@ cd
             <td>{{ $task->period }}
             <td>{{ $task->getPriorityString() }}
             <td><a href="{{ route('detail', ['task_id' => $task->id]) }}">詳細閲覧</a>
-            <td><a href="./edit.html">編集</a>
+            <td><a href="{{ route('edit', ['task_id' => $task->id]) }}">編集</a>
             <td><form action="./top.html"><button>完了</button></form>
 @endforeach
         </table>
